e41e78ae1a2bc92175101b5a9ed312642153e7f0
Ahmed Yagoub <ahyagoub40@gmail.com>
2020-02-19 18:27:28 -0700

    used hooks to transition between modes, back and forth

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+export function useVisualMode(initialMode) {
+  const [mode, setMode] = useState(initialMode);
+  const [history, setHistory] = useState([initialMode]);
+  const transition = (nextMode, replace = false) => {
+    if (replace) {
+      history.pop();
+      setMode(nextMode);
+    } else {
+      setHistory(history.concat(nextMode));
+      setMode(nextMode);
+    }
+  };
+  const back = () => {
+    if (history.length > 1) {
+      history.pop();
+    }
+    const prevMode = history[history.length - 1];
+    setMode(prevMode);
+  };
+  return {mode, transition, back};
+}
