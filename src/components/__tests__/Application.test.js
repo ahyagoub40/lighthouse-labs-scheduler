47affe122b31b8916de451fa7e30b26bd63e028d
Ahmed Yagoub <ahyagoub40@gmail.com>
2020-02-25 13:24:51 -0700

    added the edit interview test

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -35,8 +35,9 @@ describe("Application", () => {
   it("loads data, cancels an interview and increases the spots remaining for Monday by 1", async() => {
     const { container } = render(<Application />);
     await waitForElement(() => getByText(container, "Archie Cohen"))
-    const appointments = getAllByTestId(container, "appointment");
-    const appointment = appointments[1];
+    const appointment = getAllByTestId(container, "appointment").find(
+      appointment => queryByText(appointment, "Archie Cohen")
+    );
     fireEvent.click(getByAltText(appointment, "Delete"));
     expect(getByText(appointment, "Delete the appointment?")).toBeInTheDocument();
     fireEvent.click(getByText(appointment, "Confirm"));
@@ -46,5 +47,24 @@ describe("Application", () => {
       queryByText(day, "Monday")
       );
       expect(getByText(day, "5 spots remaining")).toBeInTheDocument();
+  });
+  it("loads data, edits an interview and keeps the spots remaining for Monday the same", async() => {
+    const { container } = render(<Application />);
+    await waitForElement(() => getByText(container, "Archie Cohen"))
+    const appointment = getAllByTestId(container, "appointment").find(
+      appointment => queryByText(appointment, "Archie Cohen")
+    );
+    fireEvent.click(getByAltText(appointment, "Edit"));
+    fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+      target: { value: "Lydia Miller-Jones" }
+    });
+    fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+    fireEvent.click(getByText(appointment, "Save"));
+    expect(getByText(appointment, "Saving")).toBeInTheDocument();
+    await waitForElement(() => getByText(appointment, "Lydia Miller-Jones"));
+    const day = getAllByTestId(container, "day").find(day =>
+      queryByText(day, "Monday")
+      );
+      expect(getByText(day, "4 spots remaining")).toBeInTheDocument();
   });
 })
