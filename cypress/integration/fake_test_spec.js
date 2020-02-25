f6ed6e237cb365568e859d98189b4cbb39b0efe1
Ahmed Yagoub <ahyagoub40@gmail.com>
2020-02-25 12:31:23 -0700

    added more cypress test as well as deleted the examples folder form the integration folder that is installed with cypress

--- cypress/integration/fake_test_spec.js
+++ cypress/integration/fake_test_spec.js
@@ -1,18 +1,27 @@
-describe("Iinterviews", () => {
-  beforeEach(() => {
-    cy.visit('/');
-    cy.contains('Monday');
+describe("navigation", () => {
+  it("should visit root", () => {
+    cy.visit("/");
   });
-  it('user can create an appointment', () => {
-    cy.get('[alt="Add"]')
-      .first()
-      .click();
-    cy.get('[data-testid="student-name-input"]')
-      .type('Lydia Miller-Jones')
-    cy.get(".interviewers__item--selected").should('not.exist')
-    cy.get('.interviewers__item-image')
-      .first()
-      .click();
-    cy.get(".interviewers__item--selected").should('exist')
-  })
+  it("should navigate to Tuesday", () => {
+    cy.visit("/");
+    cy.get("li").contains("Tuesday").click();
+    cy.get("li", {value:"Tuesday"})
+      .should("have.css", "background-color", "rgb(242, 242, 242)");
+  });
 })
+    // beforeEach(() => {
+    //   cy.visit('/');
+    //   cy.contains('Monday');
+    // });
+    // it('user can create an appointment', () => {
+    //   cy.get('[alt="Add"]')
+    //     .first()
+    //     .click();
+    //   cy.get('[data-testid="student-name-input"]')
+    //     .type('Lydia Miller-Jones')
+    //   cy.get(".interviewers__item--selected").should('not.exist')
+    //   cy.get('.interviewers__item-image')
+    //     .first()
+    //     .click();
+    //   cy.get(".interviewers__item--selected").should('exist')
+    // })
