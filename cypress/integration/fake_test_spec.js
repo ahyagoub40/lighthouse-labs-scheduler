76e60a1ee2d5531ddc721cca32c80aac13d5ed4a
Ahmed Yagoub <ahyagoub40@gmail.com>
2020-02-26 20:48:51 -0700

    added screenshots and deleted comments

--- cypress/integration/fake_test_spec.js
+++ cypress/integration/fake_test_spec.js
@@ -1,22 +1,4 @@
-// if (ENV === "development" || ENV === "test") {
-//   Promise.all([
-//     read(path.resolve(__dirname, `db/schema/create.sql`)),
-//     read(path.resolve(__dirname, `db/schema/${ENV}.sql`))
-//   ])
-//     .then(([create, seed]) => {
-//       app.get("/api/debug/reset", (request, response) => {
-//         db.query(create)
-//           .then(() => db.query(seed))
-//           .then(() => {
-//             console.log("Database Reset");
-//             response.status(200).send("Database Reset");
-//           });
-//       });
-//     })
-//     .catch(error => {
-//       console.log(`Error setting up the reset route: ${error}`);
-//     });
-// }
+
 
 describe("navigation", () => {
   it("should visit root", () => {
@@ -29,19 +11,4 @@ describe("navigation", () => {
       .should("have.class", "day-list__item--selected");
   });
 })
-    // beforeEach(() => {
-    //   cy.visit('/');
-    //   cy.contains('Monday');
-    // });
-    // it('user can create an appointment', () => {
-    //   cy.get('[alt="Add"]')
-    //     .first()
-    //     .click();
-    //   cy.get('[data-testid="student-name-input"]')
-    //     .type('Lydia Miller-Jones')
-    //   cy.get(".interviewers__item--selected").should('not.exist')
-    //   cy.get('.interviewers__item-image')
-    //     .first()
-    //     .click();
-    //   cy.get(".interviewers__item--selected").should('exist')
-    // })
+   
